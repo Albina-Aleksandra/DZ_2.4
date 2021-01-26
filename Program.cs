06911fc66bfda58d9e3ef147d3f8c66d4f796690
Albina <alninayakupova9@icloud.com>
2021-01-26 14:30:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
